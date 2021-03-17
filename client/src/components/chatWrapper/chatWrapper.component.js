d3074df8aee85ee624a0d6e0844c8ecde7ac9daa
Andrew Herasimenko <fylhsqvh@gmail.com>
2021-03-17 13:21:56 +0200

    Transferred chat logic up the comp.tree

--- client/src/components/chatWrapper/chatWrapper.component.js
+++ client/src/components/chatWrapper/chatWrapper.component.js
@@ -1,97 +1,23 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
 import { useMediaQuery } from "react-responsive";
-import { setChatConnection, setOpponentID, setOpponentNickname } from "../../redux/actions";
 import { WechatOutlined } from '@ant-design/icons';
 import { Drawer, Badge } from "antd";
 import { ChatComponent } from "../../components/chatComponent/Chat.component";
-import { useBreakPoint } from "../../hooks/breakpoint.hook";
-import { useWebsocket } from "../../hooks/websocket.hook";
-import { showNotification } from "../../functions/showNotification";
-import { discardFieldChanges } from "../../functions/discardFieldchanges";
-
 import "./chatWrapper.css";
 
-export const ChatWrapper = () => {
-    const dispatch = useDispatch();
-    const opponentID = useSelector(state => state.opponent.opponentId);
-
-    const [chatLog, setChatLog] = useState(null);
-    const [hiddenMessages, setHiddenMessages] = useState(0);
-
-    const { chatShown,
-        isSmScreenSize,
-        onCloseDrawer,
-        openChat
-      } = useBreakPoint(1200);
-
-    const onMessageCallback = useCallback((msg) => {
-        const message = JSON.parse(msg.data);
-
-        switch(message.type) {
-            case "CHAT_HISTORY": 
-                setChatLog(message.payload);
-                break;
-            case "CHAT_MESSAGE":
-                setChatLog(prevState => {
-                    const newState = [...prevState];
-                    newState.push(message.payload);
-                    return newState;
-                });
-                
-                if(isSmScreenSize && !chatShown) setHiddenMessages(prev => prev + 1);
-                break;
-            default: 
-                console.log("Unknown message type!"); 
-        }
-
-    }, [isSmScreenSize, chatShown]);
-
-    const onWSClose = (event) => {
-        if(event.code === 4000) {
-            showNotification({
-                message: "You won",
-                description: "Your opponent has abandoned the game",
-                type: "success"
-            })
-        dispatch(setOpponentID(null));
-        dispatch(setOpponentNickname(null));
-        discardFieldChanges().map(item => dispatch(item));
-        }
-    }
+export const ChatWrapper = ({ 
+    isSmScreenSize, 
+    hiddenMessages, 
+    onIconClickHandler,
+    chatLog,
+    onCloseDrawer,
+    chatShown
+}) => {
 
     const isWidthLess400 = useMediaQuery(
         { maxWidth: 400 }
     );
-
-    const onIconClickHandler = useCallback(() => {
-        openChat();
-        setHiddenMessages(0);
-    }, [openChat]);
     
-    const { wsConnection } = useWebsocket({ 
-        url: 'ws://localhost:5000/api/chat', 
-        name: "chatSocket",
-        onMessageCallback,
-        onWSClose,
-        opponentId: opponentID
-    });
-
-    useEffect(() => {
-        setHiddenMessages(0);
-    }, [isSmScreenSize]);
-
-    useEffect(() => {
-        if(wsConnection) dispatch(setChatConnection(wsConnection));
-    }, [wsConnection, dispatch]);
-
-    useEffect(() => {
-        return () => {
-            dispatch(setChatConnection(null));
-        }
-    }, [dispatch])
-
-
     return (
         <>
             {
--- client/src/pages/GameFieldPage/GameFieldPage.js
+++ client/src/pages/GameFieldPage/GameFieldPage.js
@@ -1,12 +1,87 @@
-import React, { useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import { setOpponentID, setOpponentNickname } from "../../redux/actions";
+import React, { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { setOpponentID, setOpponentNickname, setChatConnection } from "../../redux/actions";
 import { ChatWrapper } from "../../components/chatWrapper/chatWrapper.component";
 import { MainFieldSetup } from "../../components/MainFieldSetup/MainFieldSetup.component";
+import { useBreakPoint } from "../../hooks/breakpoint.hook";
+import { useWebsocket } from "../../hooks/websocket.hook";
+import { showNotification } from "../../functions/showNotification";
+import { discardFieldChanges } from "../../functions/discardFieldchanges";
 import "./gameField.css";
 
 export const GameFieldPage = () => {
   const dispatch = useDispatch();
+  const opponentID = useSelector(state => state.opponent.opponentId);
+  const [chatLog, setChatLog] = useState(null);
+  const [hiddenMessages, setHiddenMessages] = useState(0);
+
+  const { chatShown,
+    isSmScreenSize,
+    onCloseDrawer,
+    openChat
+  } = useBreakPoint(1200);
+
+  const onMessageCallback = useCallback((msg) => {
+    const message = JSON.parse(msg.data);
+
+    switch(message.type) {
+        case "CHAT_HISTORY": 
+            setChatLog(message.payload);
+            break;
+        case "CHAT_MESSAGE":
+            setChatLog(prevState => {
+                const newState = [...prevState];
+                newState.push(message.payload);
+                return newState;
+            });
+            
+            if(isSmScreenSize && !chatShown) setHiddenMessages(prev => prev + 1);
+            break;
+        default: 
+            console.log("Unknown message type!"); 
+    }
+
+  }, [isSmScreenSize, chatShown]);
+
+  const onWSClose = (event) => {
+    if(event.code === 4000) {
+        showNotification({
+            message: "You won",
+            description: "Your opponent has abandoned the game",
+            type: "success"
+        })
+    dispatch(setOpponentID(null));
+    dispatch(setOpponentNickname(null));
+    discardFieldChanges().map(item => dispatch(item));
+    }
+  }
+
+  const onIconClickHandler = useCallback(() => {
+    openChat();
+    setHiddenMessages(0);
+  }, [openChat]);
+
+  const { wsConnection } = useWebsocket({ 
+    url: 'ws://localhost:5000/api/chat', 
+    name: "chatSocket",
+    onMessageCallback,
+    onWSClose,
+    opponentId: opponentID
+  });
+
+  useEffect(() => {
+    setHiddenMessages(0);
+  }, [isSmScreenSize]);
+
+  useEffect(() => {
+    if(wsConnection) dispatch(setChatConnection(wsConnection));
+  }, [wsConnection, dispatch]);
+
+  useEffect(() => {
+    return () => {
+        dispatch(setChatConnection(null));
+    }
+  }, [dispatch])
 
   useEffect(() => {
     return () => {
@@ -21,7 +96,14 @@ export const GameFieldPage = () => {
                 <MainFieldSetup />
               </div>
 
-              <ChatWrapper />
+              <ChatWrapper
+                isSmScreenSize={isSmScreenSize}
+                hiddenMessages={hiddenMessages}
+                onIconClickHandler={onIconClickHandler}
+                chatLog={chatLog}
+                onCloseDrawer={onCloseDrawer}
+                chatShown={chatShown}
+              />
               
             </div>  
       );
