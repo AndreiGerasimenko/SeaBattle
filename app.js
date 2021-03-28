37d4616428d2dd93c31bd25ddb667c388b16e0b6
Andrew Herasimenko <fylhsqvh@gmail.com>
2021-03-28 15:12:59 +0300

    deleted one row

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ async function start() {
     });
     app.listen(PORT, () => {
       console.log(`Server has been started on port ${PORT}...`);
-      console.log('process.env.NODE_ENV', process.env.NODE_ENV)
     });
   } catch (error) {
     console.log("Server error", error.message);
